3677c734d75d225153bb689ca6a5001f502acc6f
yozakura <matchnokaori@gmail.com>
2022-01-04 19:16:18 +0900

    modal success

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
   <h1>{{ msg }}</h1>
-  <Modal />
+  <div v-if="showModal">
+    <Modal :title="title" :text="text" theme="sale" @close="toggleModal" />
+    <Signup />
+  </div>
+  <br>
+  <button @click="toggleModal">ログイン</button>
+  <button @click="toggleModal">新規登録</button>
 </template>
 
 <script>
 import Modal from "./components/Modal.vue";
+import Signup from "./components/Signup.vue";
 
 export default {
   name: 'App',
   components: {
-    Modal,
+    Modal, Signup,
   },
   data() {
     return {
+      showModal: false,
       msg: "初めてのVueアプリです(../",
+      title: "modal title",
+      text: "モーダルのコンテンツが入ります",
     }
   },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
 }
 </script>
 
