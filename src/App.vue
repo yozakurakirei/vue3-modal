0f093422b72f56636563b688aa5bbb459f38170b
yozakura <matchnokaori@gmail.com>
2022-01-04 17:39:15 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <Modal />
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      msg: "初めてのVueアプリです(../",
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 1rem;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>モーダルのコンテンツがここに入ります</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+    .modal {
+      width: 40rem;
+      padding: 2rem;
+      margin: 10rem auto;
+      background: white;
+      border-radius: 1rem;
+    }
+  }
+</style>
+
+
+
